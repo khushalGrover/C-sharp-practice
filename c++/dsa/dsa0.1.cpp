b52f2fdb98efcad09fac090619c30f655c7ab455
Khushal Grover <khushal15grover@gmail.com>
2022-05-06 01:03:50 +0530

    basic parctice of for loop

---
+++ c++/dsa/dsa0.1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /* checking size of veribles */
+    int a;
+    char b;
+    a=13;
+    cout<<"size of int "<< sizeof(a)<<endl;
+    cout<<"size of int "<< sizeof(b)<<endl;
+    
+    
+
+    return 0;
+}
---
+++ c++/dsa/snakeGame.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+   
+    
+    int a,b=10;
+    cout<<"working this part";
+    for(int i=0;i<=b;i++)
+    {
+        cout<<"I am hacking your pc "<< i <<" timed"<<endl;
+     
+    }
+
+    
+    
+    cout<<"program ended";
+    cin>>a;
+    return 0;
+}
+
