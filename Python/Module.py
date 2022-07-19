5e39f34f134bf793ada77d53236a3d7325e0495a
khushalGrover <khushal15grover@gmail.com>
2022-07-19 18:45:24 +0530

    some math functions

--- Python/Module.py
+++ Python/Module.py
@@ -5,7 +5,7 @@ import math
 print(math.ceil(2.888888))
 # ceil roundoff number to previ. nearest int
 print(math.floor(2.888888))
-# other funtions...
+# other funtions...dff
 print(math.factorial(4))
 print(math.pow(2,3))
 print(math.gcd(2,20))
