4511697146b3c5784e522501374c09ee7df635e6
khushalGrover <khushal15grover@gmail.com>
2022-07-26 22:44:50 +0530

    Day01 Done
    completed day one challenge
    > hello world a simple print program
    > input function
    > Variable assining
    > Brand generator is program in which two strings are join together

---
+++ Python/100_Days_Practices/Day01/BrandGenaerator.py
@@ -0,0 +1,13 @@
+#1. Create a greeting for your progress
+print("welcome to the brand genrated program, please enter some details:")
+
+#2. Ask the user for the city that they arew up in.
+cityName = input("Enter the city name in which you born:\n")
+
+#3. Ask the user for the Name of pet
+petName = input("Enter your pet name:\n")
+
+# 4. Combine the name of their city and pet and show the their band name.
+brandName = cityName + petName
+print("Your Brand name is: " + brandName)
+#5. Make sure the input cursor shows on a line.
